00c7fd6617408608f2150a7b09fa92cb69beea73
CODERON (PTY) LTD <ronnienyamhute81@gmail.com>
2025-07-18 15:13:11 +0200

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export type Vehicle = {
   id: string;
   slug: string;
@@ -22,29 +21,3 @@ export type Booking = {
   pickup: string;
   dropoff: string;
   status: "pending" | "confirmed" | "cancelled";
-=======
-export type Vehicle = {
-  id: string;
-  slug: string;
-  name: string;
-  seats: number;
-  luggage: number;
-  image: string;
-  dayRate: number;
-  description: string;
-  features: string[];
-};
-
-export type Booking = {
-  id:string;
-  vehicleId: string;
-  start: string; // ISO
-  end: string;   // ISO
-  name: string;
-  email: string;
-  phone: string;
-  pickup: string;
-  dropoff: string;
-  status: "pending" | "confirmed" | "cancelled";
->>>>>>> d838527dfb3712e23902bb78922705722be566fb
-};

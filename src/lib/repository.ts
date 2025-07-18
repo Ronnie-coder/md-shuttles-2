a445492bb5ddbd33ce5800511680e9604fbb99b3
CODERON (PTY) LTD <ronnienyamhute81@gmail.com>
2025-07-18 15:08:38 +0200

    Update repository.ts

--- src/lib/repository.ts
+++ src/lib/repository.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Vehicle } from "@/types";
 import mockVehicles from '@/data/mockVehicles.json';
 
@@ -11,20 +10,3 @@ export const vehicleRepository = {
     return Promise.resolve(mockVehicles.find(v => v.slug === slug));
   },
 };
-
-=======
-import { Vehicle } from "@/types";
-import mockVehicles from '@/data/mockVehicles.json';
-
-// --- Vehicle Repository ---
-export const vehicleRepository = {
-  findAll: async (): Promise<Vehicle[]> => {
-    return Promise.resolve(mockVehicles);
-  },
-  findBySlug: async (slug: string): Promise<Vehicle | undefined> => {
-    return Promise.resolve(mockVehicles.find(v => v.slug === slug));
-  },
-};
-
->>>>>>> d838527dfb3712e23902bb78922705722be566fb
-// We will build out the booking repository later when we add the widget

9db32f63e4b12e27be637fdfaf318e788d791b27
CODERON (PTY) LTD <ronnienyamhute81@gmail.com>
2025-07-18 15:13:53 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
@@ -7,14 +6,3 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
   },
-=======
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-  },
->>>>>>> d838527dfb3712e23902bb78922705722be566fb
-});
